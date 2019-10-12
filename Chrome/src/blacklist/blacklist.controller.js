b3737f6880420f05d9b89ff207777ead30eadd75
Genti Elezaj <gogi_46@live.com>
2019-10-12 15:15:09 +0200

    added black list front

--- Chrome/src/blacklist/blacklist.controller.js
+++ Chrome/src/blacklist/blacklist.controller.js
@@ -1,8 +1,24 @@
-wwapp.controller("BlacklistController", function ($scope, $location) {
+wwapp.controller("BlacklistController", function ($scope, $location, $notification) {
     var vm = this;
     vm.title = "Blacklist";
 
+    vm.blacklist = {
+        data: [{
+            name: 'account.google.com',
+            password: true
+        }, {
+            name: 'name1'
+        }],
+        total: 2,
+        loading: false
+    };
+
     vm.goToSettings = function () {
         $location.path('settings');
+    };
+
+    vm.save = function (item) {
+        console.log(item);
+        $notification.success('TODO: implement save');
     };
 });
